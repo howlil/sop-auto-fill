524f37aaa464669d727479d85f08b80d7a3f9418
Mhd Ulil Abshar <87646428+howlil@users.noreply.github.com>
2026-08-12 13:41:10 +0700

    refactor: scope pelaksana to workspace

--- server/src/modules/sop/pelaksana/pelaksana.module.ts
+++ server/src/modules/sop/pelaksana/pelaksana.module.ts
@@ -1,12 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from '../../core/auth/auth.module';
-import { OpdModule } from '../../core/opd/opd.module';
+import { WorkspaceModule } from '../../workspace/workspace.module';
 import { PelaksanaController } from './pelaksana.controller';
 import { PelaksanaRepository } from './pelaksana.repository';
 import { PelaksanaService } from './pelaksana.service';
 
 @Module({
-  imports: [AuthModule, OpdModule],
+  imports: [AuthModule, WorkspaceModule],
   controllers: [PelaksanaController],
   providers: [PelaksanaService, PelaksanaRepository],
 })
