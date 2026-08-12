2f7e86d9c0b53c95191f3569c47aafb7243ff969
Mhd Ulil Abshar <87646428+howlil@users.noreply.github.com>
2026-08-12 13:40:46 +0700

    refactor: bind SOP procedure to workspace ownership

--- server/src/modules/sop/prosedur/sop-prosedur.module.ts
+++ server/src/modules/sop/prosedur/sop-prosedur.module.ts
@@ -1,13 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from '../../core/auth/auth.module';
-import { OpdModule } from '../../core/opd/opd.module';
+import { WorkspaceModule } from '../../workspace/workspace.module';
 import { SopCatalogModule } from '../catalog/sop-catalog.module';
 import { SopProsedurController } from './sop-prosedur.controller';
 import { SopProsedurRepository } from './sop-prosedur.repository';
 import { SopProsedurService } from './sop-prosedur.service';
 
 @Module({
-  imports: [AuthModule, OpdModule, SopCatalogModule],
+  imports: [AuthModule, WorkspaceModule, SopCatalogModule],
   controllers: [SopProsedurController],
   providers: [SopProsedurService, SopProsedurRepository],
 })
