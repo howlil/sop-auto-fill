1d4c7e4c61a6fc053f7a915896bd435997b74fc1
Mhd Ulil Abshar <87646428+howlil@users.noreply.github.com>
2026-08-12 14:14:24 +0700

    refactor: remove password and role auth leftovers

--- server/src/common/auth/password.constants.ts
+++
@@ -1,5 +0,0 @@
-/** Rounds bcrypt untuk hash kata sandi pengguna. */
-export const BCRYPT_SALT_ROUNDS = 10;
-
-/** Kata sandi awal saat admin membuat akun peran (penyusun, evaluator, kepala OPD). */
-export const DEFAULT_PENGGUNA_PASSWORD = '@Password123:)';
