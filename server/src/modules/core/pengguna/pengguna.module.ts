aaf320c11f8301e43b5fa5b60b504e4082149fae
Mhd Ulil Abshar <87646428+howlil@users.noreply.github.com>
2026-08-12 14:12:29 +0700

    refactor: remove legacy evaluation and access domains

--- server/src/modules/core/pengguna/pengguna.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { PenggunaRepository } from './pengguna.repository';
-
-@Module({
-  providers: [PenggunaRepository],
-  exports: [PenggunaRepository],
-})
-export class PenggunaModule {}
